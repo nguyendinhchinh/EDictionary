e03152137fde7527b8d967532e07736c1aac03c8
Near Huscarl <near.huscarl@gmail.com>
2018-01-28 17:50:21 +0700

    add prefix_search_binary_search.py

---
+++ prefix_search_binary_search.py
@@ -0,0 +1,85 @@
+#!/bin/env python
+
+""" Test search prefix using binary search """
+
+import os
+import bisect
+import curses
+from pprint import pprint as p
+
+def get_word_list(path):
+	""" Get word list from file """
+	with open(path, 'r') as file:
+		words = [word.strip() for word in file.readlines()]
+		return words
+
+def binary_search(sequence, element):
+	""" original and classic binary search """
+	min = 0
+	max = len(sequence) - 1
+	while True:
+		if max < min:
+			return -1
+		m = (min + max) // 2
+		if sequence[m] == element:
+			return m
+		elif sequence[m] < element:
+			min = m + 1
+		elif sequence[m] > element:
+			max = m - 1
+
+def prefix_search(element, sequence):
+	""" Prefix search using binary search algorithm """
+	min = 0
+	max = len(sequence) - 1
+	prefix_index = -1
+	while True:
+		if max < min:
+			if prefix_index != -1:
+				return prefix_index
+			return -1
+		m = (min + max) // 2
+		if sequence[m].startswith(element):
+			prefix_index = m
+		if sequence[m] < element:
+			min = m + 1
+		elif sequence[m] > element:
+			max = m - 1
+		elif sequence[m] == element:
+			return m
+
+def main(win):
+	# setup english dictionary
+	cwd = os.getcwd()
+	english_words_path = os.path.join(cwd, 'english_words.txt')
+	english_words = get_word_list(english_words_path)
+
+	word = ''
+	search_index = -1
+	win.clear()
+	win.addstr('Search: ')
+	while True:
+		try:
+			key = win.getkey()
+			if key in ('KEY_BACKSPACE', '\b', '\x7f'):
+				word = word[:-1] # delete last character
+			else:
+				word += str(key)
+
+			search_index = prefix_search(word, english_words)
+			win.clear()
+			win.addstr('Search: ' + word + "\n")
+			for i in range(20):
+				win.addstr(english_words[search_index + i] + "\n")
+			if key == os.linesep:
+				win.addstr('You typed: ' + word)
+				key = win.getch()
+				break
+		except Exception as e:
+			# No input
+			pass
+
+if __name__ == '__main__':
+	curses.wrapper(main)
+
+# vim: nofoldenable
