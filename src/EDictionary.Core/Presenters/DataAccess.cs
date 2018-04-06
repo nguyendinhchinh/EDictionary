edd7e904dc0d931c5aa721d7462713f14d890420
Near <16520846@gm.uit.edu.vn>
2018-04-06 19:08:46 +0700

    add utility project to convert json data -> db (sqlite)

--- src/EDictionary.Core/Presenters/DataAccess.cs
+++ src/EDictionary.Core/Presenters/DataAccess.cs
@@ -1,33 +1,94 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EDictionary.Core.Utilities;
+using EDictionary.Core.Models;
 
 namespace EDictionary.Core.Presenters
 {
-    public static class DataAccess
-    {
-        private static SQLiteConnection SQLiteConnection;
+	public static class DataAccess
+	{
+		private static readonly string saveDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\EDictionary\Data");
+		private static readonly string savePath =  Path.GetFullPath($"{saveDir}\\words.sqlite");
+		private static readonly string connectionStr = $"Data Source={savePath};Version=3;";
 
-        public static void Init(string connectionStr, string filename)
-        {
-            SQLiteConnection.CreateFile(filename);
-            SQLiteConnection = new SQLiteConnection(connectionStr);
-            SQLiteConnection.Open();
-        }
+		private static readonly string tablename = "wordlist";
+		private static readonly string insertQuery = $"INSERT INTO {tablename} (WordID, Definition) VALUES (@wordID, @definition)";
+		private static readonly string createTableQuery = $"CREATE TABLE IF NOT EXISTS {tablename} (WordID NVARCHAR, Definition NVARCHAR)";
 
-        public static void Execute(string cmd)
-        {
-            SQLiteCommand command = new SQLiteCommand(cmd, SQLiteConnection);
-            command = new SQLiteCommand(cmd, SQLiteConnection);
-            command.ExecuteNonQuery();
-        }
+		private static SQLiteConnection dbConnection;
 
-        public static void Insert(string table, string values)
-        {
-            Execute("insert into " + table + " " +  values);
-        }
-    }
+		// Static class dont have constructor :(
+		public static void Create()
+		{
+			if (!File.Exists(savePath))
+			{
+				SQLiteConnection.CreateFile(savePath);
+			}
+			dbConnection = new SQLiteConnection(connectionStr);
+			CreateTable();
+		}
+
+		private static void OpenConnection()
+		{
+			if (dbConnection.State != ConnectionState.Open)
+				dbConnection.Open();
+		}
+		private static void CloseConnection()
+		{
+			if (dbConnection.State != ConnectionState.Closed)
+				dbConnection.Close();
+		}
+
+		public static void CreateTable()
+		{
+			try
+			{
+				using (SQLiteCommand command = new SQLiteCommand(createTableQuery, dbConnection))
+				{
+					OpenConnection();
+					command.ExecuteNonQuery();
+				}
+			}
+			catch (Exception ex)
+			{
+				// TODO
+			}
+			finally
+			{
+				CloseConnection();
+			}
+		}
+		public static void Insert(string wordJsonStr)
+		{
+			try
+			{
+				using (SQLiteCommand command = new SQLiteCommand(insertQuery, dbConnection))
+				{
+					OpenConnection();
+
+					Word word = JsonHelper.Deserialize(wordJsonStr);
+
+					command.Parameters.Add(new SQLiteParameter() {ParameterName = "@wordID",     Value = word.Keyword});
+					command.Parameters.Add(new SQLiteParameter() {ParameterName = "@definition", Value = wordJsonStr});
+
+					command.ExecuteNonQuery();
+				}
+			}
+			catch (Exception ex)
+			{
+				/* LogWriter.Instance.WriteLine(String.Format("Error occured at SaveVocable in DataBaseAccess:\n{0}", ex.Message)); */
+				/* return new Result<int>(ex.Message, "", Status.Error, ex); */
+			}
+			finally
+			{
+				CloseConnection();
+			}
+		}
+	}
 }
