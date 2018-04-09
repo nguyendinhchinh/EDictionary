c6ddbefa09aafd1780459addf550550b51c9e1a1
Near <16520846@gm.uit.edu.vn>
2018-04-09 16:21:53 +0700

    add interface for GUI and some basic logic

--- src/EDictionary.Core/Presenters/EDictionaryLib.cs
+++ src/EDictionary.Core/Presenters/EDictionaryLib.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using EDictionary.Core.Models;
+using EDictionary.Core.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,52 @@ using System.Threading.Tasks;
 
 namespace EDictionary.Core.Presenters
 {
-	class EDictionaryLib
+	public class EDictionaryLib
 	{
+		private IEDictionary eDictionaryView;
+
+		public EDictionaryLib(IEDictionary view)
+		{
+			DataAccess.Create();
+			eDictionaryView = view;
+		}
+
+		public void InitWordList()
+		{
+			List<string> words = DataAccess.GetWordList();
+			eDictionaryView.WordList = words;
+		}
+
+		/// <summary>
+		/// Print all info about a word on view
+		/// </summary>
+		public void GetDefinition(string wordID)
+		{
+			Word word = DataAccess.LookUp(wordID);
+
+			if (word == null)
+			{
+				// some word have multiple form like "truck_1" (noun) and "truck_2" (verb)
+				word = DataAccess.LookUp(wordID + "_1");
+
+				if (word == null)
+				{
+					IEnumerable<string> candidates = SpellCheck.Candidates(word.word);
+
+					foreach (var candidate in candidates)
+					{
+						eDictionaryView.Definition = candidate;
+					}
+				}
+				else
+				{
+					eDictionaryView.Definition = word.ToString();
+				}
+			}
+			else
+			{
+				eDictionaryView.Definition = word.ToString();
+			}
+		}
 	}
 }
