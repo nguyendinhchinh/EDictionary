9fd2dfc71d169d568b680879470a664cc2482672
haulinh <haulinhlk@gmail.com>
2018-04-09 14:37:29 +0700

    xoa het roi

--- src/EDictionary.Core/Models/Reference.cs
+++ src/EDictionary.Core/Models/Reference.cs
@@ -4,11 +4,5 @@ namespace EDictionary.Core.Models
 	{
 		public string Keyword { get; set; }
 		public string Text { get; set; }
-
-		public void print()
-		{
-			System.Console.WriteLine($"Keyword: {Keyword}");
-			System.Console.WriteLine($"Text: {Text}");
-		}
 	}
 }
