a1e193f04945164bb826b325a676cdea4a04891e
haulinh <haulinhlk@gmail.com>
2018-04-09 14:29:39 +0700

    add DataAccess.LookUp() and DataAccess.WordList()

---
+++ src/EDictionary.Core/Data/WordTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDictionary.Core.Data
+{
+	public static class WordTable
+	{
+		public const string WordID = "WordID";
+		public const string Definition = "Definition";
+		public const string TableName = "wordlist";
+	}
+}
