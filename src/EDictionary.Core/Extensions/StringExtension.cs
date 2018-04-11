491c8fc6982371c23911df8eff9f245d0ac0b259
Near <16520846@gm.uit.edu.vn>
2018-04-12 02:48:43 +0700

    fix autocomplete mismatch with multi-wordform word like run_1 & run_2
    solution: listbox now only show one word which will always be word_1
    if this word appear multiple time because it has multiple wordforms
    word_2 and others can be found in other results list (not implement yet)

---
+++ src/EDictionary.Core/Extensions/StringExtension.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EDictionary.Core.Extensions
+{
+	public static class StringExtension
+	{
+		/// <summary>
+		/// "time_1" -> "time"
+		/// </summary>
+		public static string StripWordNumber(this string str)
+		{
+			return Regex.Replace(str, @"_[0-9]$", string.Empty);
+		}
+
+		/// <summary>
+		/// "time" -> "time_1" (by default)
+		/// </summary>
+		public static string AppendWordNumber(this string str, int wordIDNumber = 1)
+		{
+			return str + $"_{wordIDNumber}";
+		}
+	}
+}
