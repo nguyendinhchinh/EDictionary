9a6ac4b008647e7315cb730cefa8d7d9ea127ccb
NearHuscarl <16520846@gm.uit.edu.vn>
2018-04-17 19:55:42 +0700

    0.1.0: add bumpversion tool to auto update version number
    populate commit msg with current version
    detect empty commit msg to abort if there is only populated version in the commit msg

--- .hooks/version.sh
+++ .hooks/version.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-VERSION_REGEX='[0-9]\+\.[0-9]\+\.[0-9]\+' # seemver
-CURRENT_VERSION=$(cat '.hooks/version' | head -n 1)
+CURRENT_VERSION="$(grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' setup.cfg)"
