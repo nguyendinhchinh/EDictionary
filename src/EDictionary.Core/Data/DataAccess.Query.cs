45af9873fa0c284be158a6bdcfff0c1239be620b
NearHuscarl <16520846@gm.uit.edu.vn>
2018-04-29 00:16:19 +0700

    1.0.7: seperate dataaccess query into another file

---
+++ src/EDictionary.Core/Data/DataAccess.Query.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDictionary.Core.Data
+{
+	public partial class DataAccess
+	{
+		private readonly string insertQuery = $@"
+			INSERT INTO {WordTable.TableName} (WordID, Definition)
+			VALUES (@wordID, @definition)";
+
+		private readonly string createTableQuery = $@"
+			CREATE TABLE IF NOT EXISTS {WordTable.TableName} (WordID NVARCHAR, Definition NVARCHAR)";
+
+		private readonly string selectDefinitionQuery = $@"
+			SELECT * FROM {WordTable.TableName}
+			WHERE {WordTable.WordID} = @wordID";
+
+		private readonly string selectWordListQuery = $@"
+			SELECT {WordTable.WordID}
+			FROM {WordTable.TableName}";
+	}
+}
