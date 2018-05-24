953cbee1980606c4ebb47fa9e3af54e85fa229d8
NearHuscarl <16520846@gm.uit.edu.vn>
2018-05-24 19:58:52 +0700

    1.6.1: add empty pop up window

---
+++ src/EDictionary.Core/Views/PopupWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using EDictionary.Core.ViewModels;
+using System.Windows;
+
+namespace EDictionary.Core.Views
+{
+	/// <summary>
+	/// Interaction logic for frmPopUp.xaml
+	/// </summary>
+	public partial class PopupWindow : Window
+	{
+		public PopupWindow()
+		{
+			InitializeComponent();
+
+			var viewModel = new PopupViewModel();
+
+			DataContext = viewModel;
+		}
+	}
+}
