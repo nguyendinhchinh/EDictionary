1c4d627e8fbbdf8fc6dfa029101e69b0d5cd5e16
haulinh <haulinhlk@gmail.com>
2018-05-25 21:03:14 +0700

    redesign MainWindow and change color title bar AboutWindow

--- src/EDictionary.Core/Views/MainWindow.xaml.cs
+++ src/EDictionary.Core/Views/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using EDictionary.Core.ViewModels.MainViewModel;
 using System;
+using System.Windows;
 
 namespace EDictionary.Core.Views
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow 
+	public partial class MainWindow : Window
 	{
 		private MainViewModel viewModel;
 
