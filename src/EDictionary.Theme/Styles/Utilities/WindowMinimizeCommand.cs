e255f667e34d1f75a1493f12c6fdfc1181806753
NearHuscarl <16520846@gm.uit.edu.vn>
2018-06-01 01:15:03 +0700

    1.8.12: restore win10 effect when minimizing window

--- src/EDictionary.Theme/Styles/Utilities/WindowMinimizeCommand.cs
+++ src/EDictionary.Theme/Styles/Utilities/WindowMinimizeCommand.cs
@@ -4,24 +4,32 @@ using System.Windows.Input;
 
 namespace Delete.Styles.Utilities
 {
-    public class WindowMinimizeCommand :ICommand
-    {     
+	public class WindowMinimizeCommand : ICommand
+	{
 
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
 
-        public event EventHandler CanExecuteChanged;
+		public event EventHandler CanExecuteChanged;
 
-        public void Execute(object parameter)
-        {
-            var window = parameter as Window;
+		public void Execute(object parameter)
+		{
+			var window = parameter as Window;
 
-            if (window != null)
-            {
-                window.WindowState = WindowState.Minimized;
-            }
-        }
-    }
+			if (window != null)
+			{
+				// If you use customized window by setting WindowStyle to None, the
+				// minimize button will still minimize the window but without the
+				// minimize animation. Instead it just instantly hides the
+				// window. This can be fix as a hack by setting WindowStyle to
+				// normal (SingleBorderWindow) the moment before executing minimize
+				// action on the window
+				window.WindowStyle = WindowStyle.SingleBorderWindow;
+
+				window.WindowState = WindowState.Minimized;
+			}
+		}
+	}
 }
